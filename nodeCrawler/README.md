9313b775f21be22199e082031b2e391aed51913e
517701884@qq.com <517701884@qq.com>
2016-04-26 14:12:05 +0800

    img crawler

---
+++ nodeCrawler/README.md
@@ -0,0 +1,16 @@
+时间获取
+	var fileName = date.getFullYear().toString() + (date.getMonth()+1) + date.getDate() + date.getHours() +date.getMinutes() + date.getSeconds() + date.getMilliseconds();
+path当前文件下
+	path.join(__dirname,name)
+request文件传输
+	request(url).pipe(fs.createWriteStream('doodle.png'))
+fs 
+====
+#创建文件夹
+	fs.mkdirSync(path);
+	fs.writeFileSync(path, data);
+#是否有该文件夹存在
+	fs.existsSync('img')
+#注意编码一致
+	req.setEncoding("binary");
+	fs.writeFileSync(imgUrlName, chunk, "binary");
---
+++ nodeCrawler/server.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path')
+var url = 'http://tieba.baidu.com/p/4500671196';
+var $;
+
+var imgCrawler = function(url){
+	if(!fs.existsSync('img')){
+		fs.mkdirSync('img');
+	}
+	var date = new Date();
+	var fileName = date.getFullYear().toString() + (date.getMonth()+1) + date.getDate() + date.getHours() +date.getMinutes() + date.getSeconds() + date.getMilliseconds();
+	fs.mkdirSync('img/' + fileName);
+	console.log('succeed create ' + fileName);
+	var html = ''
+	http.get(url, function(req){
+		console.log('正在爬取 ' + url + '...........');
+		req.on('data', function(data){
+			html += data;
+		});
+
+		req.on('end', function(){
+			$ = cheerio.load(html);
+			
+			$('img').each(function(index){
+				var imgUrl = $(this).attr('src');
+				var imgName = imgUrl.split('/').pop();
+				var imgType = imgName.split('.').pop();
+				var imgUrlName = path.join(__dirname, 'img/' + fileName + '/' + index + '.' + imgType);
+				if(imgType == 'png' || imgType == 'jpg' || imgType == 'gif'){
+					http.get(imgUrl, function(req){
+						req.setEncoding("binary");
+						var chunk = '';
+						req.on('data', function(data){
+							chunk += data;
+						});
+
+						req.on('end', function(){
+							fs.writeFileSync(imgUrlName, chunk, "binary");
+						});
+					});
+				}
+			});
+
+		});
+	});
+	console.log('succeed')
+}
+
+imgCrawler(url);
